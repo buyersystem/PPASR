a59ee5f20508c662f5a9a8d3abcc76a906ea3998
夜雨飘零 <yeyupiaoling@foxmail.com>
2025-02-20 20:52:25 +0800

    修复类型转换失败

--- ppasr/trainer.py
+++ ppasr/trainer.py
@@ -30,7 +30,7 @@ from ppasr.optimizer import build_lr_scheduler, build_optimizer
 from ppasr.utils.checkpoint import save_checkpoint, load_pretrained, load_checkpoint
 from ppasr.utils.metrics import cer, wer, mer
 from ppasr.utils.summary import summary
-from ppasr.utils.utils import dict_to_object, print_arguments
+from ppasr.utils.utils import dict_to_object, print_arguments, convert_string_based_on_type
 from ppasr.data_utils.utils import create_manifest, merge_audio
 
 
@@ -75,12 +75,13 @@ class PPASRTrainer(object):
         if overwrites:
             overwrites = overwrites.split(",")
             for overwrite in overwrites:
-                keys, v = overwrite.strip().split("=")
+                keys, value = overwrite.strip().split("=")
                 attrs = keys.split('.')
                 current_level = self.configs
                 for attr in attrs[:-1]:
                     current_level = getattr(current_level, attr)
-                setattr(current_level, attrs[-1], eval(v))
+                before_value = getattr(current_level, attrs[-1])
+                setattr(current_level, attrs[-1], convert_string_based_on_type(before_value, value))
         # 打印配置信息
         print_arguments(configs=self.configs)
         # 读取数据增强配置文件
